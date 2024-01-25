60692a2f4f3075c7c6ad3401bd5837f70a428288
codetive3ankit@gmail.com <codetive3ankit@gmail.com>
2024-01-25 16:12:03 +0530

    first git

---
+++ THEME CHANGE/script.js
@@ -0,0 +1,17 @@
+ 
+  var darkMode = false;
+
+    function dark() {
+      var body = document.body;
+      darkMode = !darkMode;
+
+      if (darkMode) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+      } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+      }
+    }
+  
+  
